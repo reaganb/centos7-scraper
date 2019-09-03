56fef01701f0ca2f304fd36d812782bd501b5bd3
Reagan Balongcas <reagan_balongcas@trendmicro.com>
2019-09-03 17:17:30 +0800

    Updated test cases

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -4,6 +4,7 @@ from scraper.scraper import Scraper
 from scraper.scraper import config_parser
 from urllib.parse import urljoin
 
+
 class TestScraper(unittest.TestCase):
 
     def setUp(self):
@@ -29,6 +30,8 @@ class TestScraper(unittest.TestCase):
 
         url = urljoin(self.base_url,'centos/7/atomic/x86_64/Packages/')
         self.assertIsNotNone(self.scrape.get_url(url))
+        url2 = urljoin(self.base_url, 'centos/7/atomic/x86_644/Packages/')
+        self.assertIsNot(int,self.scrape.get_url(url2))
 
     def test_scrape_files_url(self):
         """
@@ -59,6 +62,8 @@ class TestScraper(unittest.TestCase):
 
         url = urljoin(self.base_url, 'centos/7/configmanagement/')
         self.assertIsNone(self.scrape.scrape_all_url(url))
+        wrong_url = 'https://wrong.web.site/centos/7/configmanagement/'
+        self.assertIsNotNone(self.scrape.scrape_all_url(wrong_url))
 
     def test_config_files(self):
         """
@@ -67,6 +72,37 @@ class TestScraper(unittest.TestCase):
 
         self.assertIsNotNone(config_parser('../data/config.ini'))
         self.assertIsNotNone(config_parser('../data/config_tests.ini'))
+
+        self.assertIsNone(config_parser('../data/config_tests.iniii'))
+
+    def test_db(self):
+        """
+        Testing the database methods and functions
+        """
+
+        db_cred = {
+            'host': 'localhost',
+            'user': 'postgres',
+            'password': 'wrongpassword'
+        }
+
+        scrape = Scraper(db=True, db_cred=db_cred)
+        # def __init__(self, csv_file="file.csv", db=False, db_cred=None):
+        url = 'http://mirror.rise.ph/centos/7/atomic/x86_64/Packages/'
+
+        with self.assertRaises(AttributeError):
+            scrape.write_to_db(url=url)
+
+    def test_run_scrapper(self):
+        """
+        Testing the run_scraper method from the Scraper class
+        """
+
+        url = urljoin(self.base_url, 'centos/7/configmanagement/')
+        self.assertIsNone(self.scrape.run_scraper(url))
+        wrong_url = 'https://wrong.web.site/centos/7/configmanagement/'
+        self.assertIsNotNone(self.scrape.run_scraper(wrong_url))
+
 
 
 if __name__ == '__main__':
